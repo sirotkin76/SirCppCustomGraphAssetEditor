1ea2944016e972ed3ffc22103506539f6ea601fc
72gsir <72gsir@gmail.com>
2026-04-01 04:09:24 +0300

    Add template logic V1

---
+++ Source/SirCustomAssetEditor/SirCustomAssetEditor.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SirCustomAssetEditor.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SirCustomAssetEditor, "SirCustomAssetEditor" );
